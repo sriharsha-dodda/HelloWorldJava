aed7f7f49aeefbc0ff4a38883e33640185d256ed
sriharsha-dodda <sriharshadodda@gmail.com>
2020-09-21 22:12:29 +0530

    added printing variables values

--- src/com/testcodes/example/HelloWorld.java
+++ src/com/testcodes/example/HelloWorld.java
@@ -4,9 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=10;
+		int x=10,y=20;
+		System.out.println("x="+x+" y="+y);
 		System.out.println("Hello World");
-		
+		System.out.println("x="+x+" y="+y);
 
 	}
 
