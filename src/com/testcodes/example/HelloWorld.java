d289efebecd45048157e4ae1282dc184946be4e2
sriharsha-dodda <sriharshadodda@gmail.com>
2020-09-21 13:01:04 +0530

    initial project version

---
+++ src/com/testcodes/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.testcodes.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+
+	}
+
+}
