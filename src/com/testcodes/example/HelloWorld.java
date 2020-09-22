421f661720be8d07d89d6904f2e71c416eafa0af
sriharsha-dodda <sriharshadodda@gmail.com>
2020-09-22 18:18:28 +0530

    added swapping

--- src/com/testcodes/example/HelloWorld.java
+++ src/com/testcodes/example/HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=10,y=20;
+		int x=10,y=20,z;
 		System.out.println("x="+x+" y="+y);
 		System.out.println("Hello World");
+		z=x;
+		x=y;
+		y=z;
 		System.out.println("x="+x+" y="+y);
 
 	}
